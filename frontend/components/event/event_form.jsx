df9050cd91325814005929c47b2f9b7cfd48078e
George V <georgevuong18@gmail.com>
2017-07-26 09:20:03 -0700

    create events form and image upload in progress

--- frontend/components/event/event_form.jsx
+++ frontend/components/event/event_form.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Dropzone from 'react-dropzone';
 import request from 'superagent';
+const CLOUDINARY_UPLOAD_PRESET = 'aopdku36';
+const CLOUDINARY_UPLOAD_URL = 'https://api.cloudinary.com/v1_1/dtluc0y85/image/upload';
 
 class EventForm extends React.Component {
   constructor(props) {
@@ -11,6 +13,7 @@ class EventForm extends React.Component {
       description: "",
       location: "",
       date_time: "",
+      image_url: "",
       ticket_price: 0,
       ticket_quantity: 0,
     };
@@ -18,6 +21,31 @@ class EventForm extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  onImageDrop(files) {
+    this.setState({
+      uploadedFile: files[0]
+    });
+
+    this.handleImageUpload(files[0]);
+  }
+
+  handleImageUpload(file) {
+    let upload = request.post(CLOUDINARY_UPLOAD_URL)
+                        .field('upload_preset', CLOUDINARY_UPLOAD_PRESET)
+                        .FIELD('file', file);
+
+    upload.end((err, response) => {
+      if(err) {
+        console.error(err);
+      }
+
+      if(response.body.secure_url !== "") {
+        this.setState({
+          uploadedFileCloudinaryUrl: response.body.secure_url
+        });
+      }
+    });
+  }
 // invoked immediately after a component is mounted
   // componentDidMount() {
   //   this.props.createEvent();
@@ -39,28 +67,49 @@ class EventForm extends React.Component {
     const { title, description, location, date_time, image_url, ticket_price, ticket_quantity } = this.state;
     console.log("render test");
 
+
+    <div>
+      <div className="FileUpload">
+
+      </div>
+
+      <div>
+        { this.state.uploadedFileCloudinaryUrl === "" ? null :
+        <div>
+          <p>{ this.state.uploadedFile.name }</p>
+          <img src={ this.state.uploadedFileCloudinaryUrl } />
+        </div>}
+      </div>
+      <Dropzone
+        multiple={false}
+        accept="image/*"
+        onDrop={this.onImageDrop.bind(this)}>
+        <p>Drop an image or click to select a file to upload.</p>
+      </Dropzone>
+    </div>
+
     return (
       <div className="event-form-container">
         <h3>Create an Event</h3>
 
         <form className="event-form" onSubmit={this.handleSubmit}>
-          <label>Title</label>
+          <label>TITLE</label>
           <input
             className="create-event-title"
             type="text"
             value={title}
             onChange={this.update('title')}
           />
-
-          <label>Description</label>
+        <br />
+        <label>DESCRIPTION</label>
           <input
             className="create-event-description"
             type="text"
             value={description}
             onChange={this.update('description')}
           />
-
-          <label>Location</label>
+        <br/>
+        <label>LOCATION</label>
             <input
               className="create-event-location"
               type="text"
@@ -68,14 +117,15 @@ class EventForm extends React.Component {
               onChange={this.update('location')}
             />
 
+          <br/>
           <label>Date and Time</label>
             <input
-              className="create-event-location"
+              className="create-event-datetime"
               type="text"
-              value={location}
-              onChange={this.update('location')}
+              value={date_time}
+              onChange={this.update('date_time')}
             />
-
+          <br/>
           <label>Ticket Price</label>
             <input
               className="ticket-price"
@@ -83,7 +133,7 @@ class EventForm extends React.Component {
               value={ticket_price}
               onChange={this.update('ticket_price')}
             />
-
+          <br/>
           <label>Ticket Quantity</label>
             <input
               className="ticket-quantity"
