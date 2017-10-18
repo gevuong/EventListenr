21b791134ab304b6e0ecaf14a23adcb860603fe1
George V <georgevuong18@gmail.com>
2017-10-17 22:02:52 -0700

    refactor navbar.jsx

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -1,122 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { withRouter } from 'react-router';
 
-class NavBar extends React.Component {
+// var rightNavbar;
+class NavBar extends Component {
   constructor(props) {
     super(props);
-
-    this.handleOnClick = this.handleOnClick.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.selectRightNavbar = this.selectRightNavbar.bind(this);
   }
 
-  handleOnClick(e) {
+  handleClick(e) {
     e.preventDefault();
     this.props.logout().then(() =>
     this.props.history.push("/"));
   }
 
-  render() {
-    if (this.props.currentUser) {
+  selectRightNavbar() {
+    const isLoggedIn = this.props.currentUser;
+
+    if (isLoggedIn) {
       return (
-        <div>
-        	<div className="header-group">
-            <nav id="login-signup">
-              <div className="eventListenr-home-container">
-                <Link to="/">Eventlistenr</Link>
-              </div>
-              <div className="session-link-container">
-                <Link to="/events/new">CREATE EVENT</Link>
-                <Link to="/profile"><i className="fa fa-user-circle" aria-hidden="true"></i>  { this.props.currentUser.username.toUpperCase() }</Link>
-                <button className="header-button" onClick={this.handleOnClick}>LOG OUT
-                </button>
-              </div>
-            </nav>
-        	</div>
+        <div className="right-navbar">
+          <Link to="/events/new">CREATE EVENT</Link>
+          <Link to="/profile"><i className="fa fa-user-circle"aria-hidden="true"></i>
+            { this.props.currentUser.username.toUpperCase() }
+          </Link>
+          <button className="header-button" onClick={this.handleClick}>LOG OUT
+          </button>
         </div>
-      );
+      )
     } else {
-        return (
-          <nav id="login-signup">
+      return (
+        <div className="right-navbar">
+          <div className="session-link-container">
+            <Link to="/login">LOGIN</Link>
+          </div>
+          <div className="session-link-container">
+            <Link to="/signup">SIGN UP</Link>
+          </div>
+        </div>
+      )
+    }
+  }
 
-            <div className="eventListenr-home-container">
-              <Link to="/">EventListenr</Link>
-
-            </div>
-            <div className="session-link-container">
-              <Link to="/login">LOGIN</Link>
-              <Link to="/signup">SIGN UP</Link>
-            </div>
-          </nav>
-        );
-      }
+  render() {
+    let rightNavbar = this.selectRightNavbar();
+    return (
+      <div className="main-navbar">
+        <div className="left-navbar">
+          <Link to="/">Eventlistenr</Link>
+        </div>
+          { rightNavbar }
+      </div>
+    );
   }
 }
 
 export default withRouter(NavBar);
-
-// const sessionLinks = () => (
-// // add auth route
-//     <nav id="login-signup">
-//
-//       <div className="eventListenr-home-container">
-//         <Link to="/">EventListenr()</Link>
-//
-//       </div>
-//       <div className="session-link-container">
-//         <Link to="/login">LOGIN</Link>
-//         <Link to="/signup">SIGN UP</Link>
-//       </div>
-//     </nav>
-// );
-//
-// // and protected route.
-//
-// const personalGreeting = (currentUser, logout) => (
-//   <div>
-//   	<div className="header-group">
-//       <nav id="login-signup">
-//         <div className="eventListenr-home-container">
-//           <Link to="/">EventListenr()</Link>
-//         </div>
-//         <div className="session-link-container">
-//           <Link to="/events/new">CREATE EVENT</Link>
-//           <Link to="/users/userId"><i className="fa fa-user-circle"  aria-hidden="true"></i></Link>
-//           <button className="header-button" onClick={ logout }>LOG OUT
-//           </button>
-//
-//         </div>
-//       </nav>
-//
-//   	</div>
-//     <div className='homepage-image'></div>
-//
-//     <div className="event-index-container">
-//       <div className="event-container">
-//         <p>Title</p>
-//         <p>Description</p>
-//         <p>Date</p>
-//         <p>Price</p>
-//       </div>
-//       <div className="event-container">
-//         <p>Title2</p>
-//         <p>Description2</p>
-//         <p>Date2</p>
-//         <p>Price2</p>
-//       </div>
-//       <div className="event-container">
-//         <p>Title3</p>
-//         <p>Description3</p>
-//         <p>Date3</p>
-//         <p>Price3</p>
-//       </div>
-//     </div>
-//   </div>
-// );
-//
-// const Greeting = ({ currentUser, logout }) => (
-//   currentUser ? personalGreeting(currentUser, logout) : sessionLinks()
-// );
-//
-// export default Greeting;
-
-      // <h3 className="header-name">Hi, {currentUser.username}!</h3>
