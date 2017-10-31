de38ddb2fc345b61dde07fcd7605668f9448ab06
George V <georgevuong18@gmail.com>
2017-10-30 23:50:59 -0700

    fix issue with highlighting entire backround of logout button in dropdown

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -52,55 +52,55 @@ class NavBar extends Component {
         <div className="left-nav">
           <Link to="/">Eventlistenr</Link>
         </div>
-          { isLoggedIn ?
-            (
-            <div className="right-nav">
-              <Link to="/events/browse">
-                <div className="navlink-container">
-                  BROWSE EVENTS
-                </div>
-              </Link>
-
-              <div className="dropdown" style={{float:"right"}}>
-                <div className='navlink-container'>
-                  <i className="fa fa-user-circle" aria-hidden="true"></i>
-                  {this.props.currentUser.username.toUpperCase()}
-                  <i className="fa fa-angle-down" aria-hidden="true"></i>
-                </div>
-                <div className="dropdown-content">
-                  <Link to="/profile">DASHBOARD</Link>
-                  <button onClick={this.handleClick}>LOG OUT
-                  </button>
-                </div>
+        { isLoggedIn ?
+          (
+          <div className="right-nav">
+            <Link to="/events/browse">
+              <div className="navlink-container">
+                BROWSE EVENTS
               </div>
+            </Link>
 
-              <Link to="/events/new">
-                <div className="create-event-link">
-                  CREATE EVENT
-                </div>
-              </Link>
+            <div className="dropdown" style={{float:"right"}}>
+              <div className='navlink-container'>
+                <i className="fa fa-user-circle" aria-hidden="true"></i>
+                {this.props.currentUser.username.toUpperCase()}
+                <i className="fa fa-angle-down" aria-hidden="true"></i>
+              </div>
+              <div className="dropdown-content">
+                <Link to="/profile">DASHBOARD</Link>
+                <button className='logout-button-container' onClick={this.handleClick}>LOG OUT
+                </button>
+              </div>
             </div>
-            ) :
-            (
-            <div className="right-nav">
-              <Link to="/login">
-                <div className="navlink-container">
-                  LOGIN
-                </div>
-              </Link>
-              <Link to="/signup">
-                <div className="navlink-container">
-                  SIGN UP
-                </div>
-              </Link>
-              <Link to="/signup">
-                <div className="create-event-link">
-                  CREATE EVENT
-                </div>
-              </Link>
-            </div>
-            )
-          }
+
+            <Link to="/events/new">
+              <div className="create-event-link">
+                CREATE EVENT
+              </div>
+            </Link>
+          </div>
+          ) :
+          (
+          <div className="right-nav">
+            <Link to="/login">
+              <div className="navlink-container">
+                LOGIN
+              </div>
+            </Link>
+            <Link to="/signup">
+              <div className="navlink-container">
+                SIGN UP
+              </div>
+            </Link>
+            <Link to="/signup">
+              <div className="create-event-link">
+                CREATE EVENT
+              </div>
+            </Link>
+          </div>
+          )
+        }
       </div>
     );
   }
