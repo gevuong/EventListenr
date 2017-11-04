05368fed01b368ed4b5ce42ec91776794e568301
George V <georgevuong18@gmail.com>
2017-11-03 23:59:46 -0700

    add time to event index

--- frontend/components/event/event_index_item.jsx
+++ frontend/components/event/event_index_item.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-class EventIndexItem extends React.Component {
+class EventIndexItem extends Component {
   constructor(props){
     super(props);
   }
 
   render() {
     const { eventItem } = this.props;
+    let date = new Date(eventItem.date_time).toDateString();
+    let time = new Date(eventItem.date_time).toTimeString().slice(0, 8);
+    console.log('parsedDate: ', eventItem);
     let eventDescription = eventItem.description.slice(0,60) + "...";
     return (
       <div className='event-index-wrapper'>
@@ -16,8 +19,9 @@ class EventIndexItem extends React.Component {
             <div className="event-container-hover"></div>
               <img src={eventItem.image_url}></img>
               <div className="eventIndex-detailsDiv">
-                <h4 className='eventTitle'>{eventItem.title}</h4>
-                <p className='eventIndexLocation'>{eventItem.location}</p>
+                <p className='datetime-p'>{ date + ' ' + time }</p>
+                <h4 className='event-title'>{eventItem.title }</h4>
+                <p className='event-index-location'>{eventItem.location}</p>
               </div>
             <br/>
             <div className="eventBookmarkDiv">
