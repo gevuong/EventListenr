4d37d9cbb7e3de66130b5cd0fc50beaf07cb87c1
George V <georgevuong18@gmail.com>
2017-12-01 12:21:15 -0800

    update redux-logger to only show in dev

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,13 +1,22 @@
 import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/root_reducer';
+import logger from 'redux-logger';
+
+
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV === 'development') {
+  const { logger } = rquire('redux-logger');
+  
+  middlewares.push(logger);
+}
 
 const configureStore = (preloadedState = {}) => (
   createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunk, logger)
+    applyMiddleware(...middlewares)
   )
 );
 
