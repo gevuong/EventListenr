a494cafef9851877e806ccf536619ecf6285b95d
George V <georgevuong18@gmail.com>
2017-12-20 22:38:45 -0800

    update webpack.config, add protectedroute

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import NavbarContainer from './navbar/navbar_container';
 import { Route, Switch } from 'react-router-dom';
 import SessionFormContainer from './session_form/session_form_container';
-import { AuthRoute } from '../util/route_util.jsx';
+import { AuthRoute, ProtectedRoute } from '../util/route_util.jsx';
 import EventFormContainer from './event/event_form_container';
 import EventShowContainer from './event/event_show_container';
 import UserProfileContainer from './user/profile_container';
@@ -14,7 +14,7 @@ const App = () => (
     <Switch>
       <Route exact path="/events/new" component={ EventFormContainer } />
       <Route path="/events/:eventId" component={ EventShowContainer } />
-      <Route path="/profile" component={ UserProfileContainer } />
+      <ProtectedRoute path="/profile" component={ UserProfileContainer } />
       <AuthRoute path="/login" component={ SessionFormContainer } />
       <AuthRoute path="/signup" component={ SessionFormContainer } />
       <Route path="/" component={ HomePage } />
