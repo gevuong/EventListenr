e853692864bc74b00c8d64c1fc6b63d4e45829a6
George V <georgevuong18@gmail.com>
2018-04-02 00:03:26 -0700

    render no events imgs and message

--- frontend/components/user/organized_events.jsx
+++ frontend/components/user/organized_events.jsx
@@ -5,15 +5,30 @@ import UpcomingEventItem from './upcoming_event_item';
 
 const OrganizedEvents = ({ organizedEvents }) => {
   console.log("organizedEvents: ", organizedEvents);
-  let organizedEvent = organizedEvents.map(event => (
-    <UpcomingEventItem eventItem={ event } key={ event.id } />
-  ))
 
-  return (
-    <ul className="organized-event-container">
-      { organizedEvent }
-    </ul>
-  );
+  if (organizedEvents.length === 0) {
+    return (
+      <div className="no-saved-events-container" id="bookmarkedEvents-container">
+        <img id="no-organized-events-img" src="https://res.cloudinary.com/dtluc0y85/image/upload/v1522652013/no_events_tokf11.png" alt="no saved events" />
+
+        <div className="text-container">
+          <p className="no-saved-events-text">
+            You do not have any organized events. Hosted events will show up here so you can easily view them here later.
+          </p>
+        </div>
+      </div>
+    )
+  } else {
+    let organizedEvent = organizedEvents.map(event => (
+      <UpcomingEventItem eventItem={ event } key={ event.id } />
+    ))
+
+    return (
+      <ul className="organized-event-container">
+        { organizedEvent }
+      </ul>
+    );
+  }
 }
 
 export default OrganizedEvents;
--- frontend/components/user/upcoming_events.jsx
+++ frontend/components/user/upcoming_events.jsx
@@ -3,14 +3,27 @@ import UpcomingEventItem from './upcoming_event_item';
 
 const UpcomingEvents = ({ ticketedEvents }) => {
     console.log("ticketedEvents: ", ticketedEvents);
-  return (
-    <ul className="ticketed-event-container">
-      { ticketedEvents.map(event => (
+
+  if (ticketedEvents.length === 0) {
+    return (
+      <div className="no-saved-events-container" id="bookmarkedEvents-container">
+        <img id="no-saved-events-img"  src="https://res.cloudinary.com/dtluc0y85/image/upload/v1512101134/calendar_iuzsmc.png" alt="no saved events" />
+
+        <div className="text-container">
+          <p className="no-saved-events-text">You do not have any upcoming events. Registered events will show up here so you can easily view them here later.</p>
+        </div>
+      </div>
+    )
+  } else {
+    return (
+      <ul className="ticketed-event-container">
+        { ticketedEvents.map(event => (
           <UpcomingEventItem eventItem={event} key={event.id} />
         ))
       }
     </ul>
-  )
+    )
+  }
 }
 
 export default UpcomingEvents;
