ef6cba0d28353160dc5b455fd8b15ac0be0952a3
George V <georgevuong18@gmail.com>
2018-04-04 22:41:12 -0700

    update babel-preset, install jquery and add "repository" field

---
+++ frontend/__tests__/entry_tests.js
@@ -0,0 +1,29 @@
+import ReactDOM from 'react-dom';
+import configureStore from '../store/store';
+
+jest.mock('../store/store', () => jest.fn(() => ({ storeKey: 'storeValue' }) ));
+
+describe('entry', () => {
+  let Entry, ReactDOM, Root;
+
+  beforeAll(() => {
+    document.addEventListener = jest.fn();
+    document.getElementById = jest.fn(id => id);
+
+    ReactDOM = require('react-dom');
+    ReactDOM.render = jest.fn();
+
+    Root = require('../components/root');
+    Entry = require('../index');
+
+    // invoke the callback passed to document.addEventListener
+    document.addEventListener.mock.calls[0][1]();
+  });
+
+  it('invokes the configureStore function', () => {
+    expect(configureStore).toBeCalled();
+  });
+
+  // Add more tests here!
+
+});
