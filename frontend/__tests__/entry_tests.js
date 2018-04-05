d7ae681917ea5b491ff1d3d14cabd03da0ca326f
George V <georgevuong18@gmail.com>
2018-04-04 23:50:26 -0700

    update react-slick, add jest config, and test-setup.js

--- frontend/__tests__/entry_tests.js
+++ frontend/__tests__/entry_tests.js
@@ -1,7 +1,11 @@
 import ReactDOM from 'react-dom';
 import configureStore from '../store/store';
 
-jest.mock('../store/store', () => jest.fn(() => ({ storeKey: 'storeValue' }) ));
+jest.mock('../store/store', () =>
+    jest.fn(() => (
+        { storeKey: 'storeValue' }
+    ))
+);
 
 describe('entry', () => {
   let Entry, ReactDOM, Root;
@@ -14,7 +18,7 @@ describe('entry', () => {
     ReactDOM.render = jest.fn();
 
     Root = require('../components/root');
-    Entry = require('../index');
+    Entry = require('../event_listenr');
 
     // invoke the callback passed to document.addEventListener
     document.addEventListener.mock.calls[0][1]();
--- frontend/event_listenr.jsx
+++ frontend/event_listenr.jsx
@@ -9,16 +9,20 @@ import Root from './components/root';
 import * as EventAPIUtil from './util/event_api_util';
 import { requestAllEvents, requestEvent } from './actions/event_actions';
 
-let store;
+
 document.addEventListener('DOMContentLoaded', () => {
-  if (window.currentUser) {
-    const preloadedState = { session: { currentUser: window.currentUser
-  } };
-    store = configureStore(preloadedState);
-    delete window.currentUser;
-  } else {
-    store = configureStore();
-  }
+    let store;
+    if (window.currentUser) {
+        const preloadedState = {
+            session: {
+                currentUser: window.currentUser,
+            }
+        };
+        store = configureStore(preloadedState);
+        delete window.currentUser;
+    } else {
+        store = configureStore();
+    }
   // // we don't put the store directly on the window because
   // // it can be confusing when debugging, sometimes giving you access to state when you shouldn't
   // window.getState = store.getState;
